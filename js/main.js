ac6197f67578ddec5c40a649e6f90a6324ca6f57
ifozest <aemaeth4@gmail.com>
2014-09-17 17:36:20 +0300

    animate improvement

--- js/main.js
+++ js/main.js
@@ -1,25 +1,32 @@
-$(function() {
+$(function () {
 
-  var processColumn = function($element, timeout, height) {
-    var colHeight = Math.round(height * Math.random());
-    $element.animate(
-      {height: colHeight},
-      timeout,
-      'linear',
-      function() {
-        processColumn($element, timeout, height);
-      });
-  };
-
-  var runEqualizer = function($selector, timeout) {
-    var height = $selector.height();
-    $selector.find('span').each(function(i, element) {
-      processColumn($(element), timeout, height);
+  var runEqualizer = function ($selector, timeout) {
+    var height = $selector.height()
+      , previousColHeights = []
+      , newColHeights = []
+      , $spans = $selector.find('span');
+    $spans.each(function (i) {
+      previousColHeights[i] = ($(this).height());
+      newColHeights[i] = (Math.round(height * Math.random()));
+    });
+    $({timer: 0}).animate({timer: 1}, {
+      duration: timeout,
+      easing: 'linear',
+      step: function (now) {
+        $spans.each(function (i) {
+          var colHeight = previousColHeights[i] + (newColHeights[i] - previousColHeights[i]) * now;
+          $(this).height(colHeight);
+        });
+      },
+      complete: function () {
+        runEqualizer($selector, timeout);
+      }
     });
   };
 
-  var setEqualizer = function(selector, timeout, colWidth) {
+  var setEqualizer = function (selector, timeout, colWidth) {
     var $selector = $(selector), colQuantity;
+
     if (!colWidth) {
       colWidth = 1;
     }
@@ -31,12 +38,12 @@ $(function() {
 
     // Кол-во столбиков
     colQuantity = Math.ceil($selector.width() / colWidth);
+
     for (var i = 0; i < colQuantity; i++) {
       $('<span/>').appendTo($selector).addClass('equalizer-span').width(colWidth);
     }
     runEqualizer($selector, timeout);
   };
-
 
   setEqualizer('#eq_1 .equalizer', 1000, 2);
   setEqualizer('#eq_2 .equalizer', 1000, 2);
